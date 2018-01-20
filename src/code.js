31c3be907d9953543cc80e5974123f121d3241b7
Daniel Serrano <danielserrano@danielserrano-macbookair.roam.corp.google.com>
2018-01-20 13:48:04 -0500

    Adding all the elements of the form to the js and also adjusting some html elements

--- src/code.js
+++ src/code.js
@@ -1,3 +1,18 @@
+// Sliders text.
+document.addEventListener("DOMContentLoaded", function(){
+var sliders = ["ownershipScaleId","ownershipSwitchId","teleScaleId","teleSwitchId"];
+
+for (var i = 0; i < sliders.length; i++) {
+  var slider = document.getElementById(sliders[i]);
+  var output = document.getElementById(sliders[i] + 'Value');
+  output.innerHTML = slider.value;
+  slider.oninput = function() {
+    var output = document.getElementById(this.id + 'Value');
+    output.innerHTML = this.value;
+  }
+}
+});
+
 function getRadioVal(form, name) {
     var val;
     var radios = form.elements[name];
@@ -13,17 +28,38 @@ function getRadioVal(form, name) {
 
 function joinText(elements){
   return elements.join('|');
-
 }
 
 function myFunction() {
   var form = document.getElementById('scoutForm');
   var elements = []
-
+  elements.push(Date.now())
   elements.push(document.getElementById("teamNumberId").value)
   elements.push(getRadioVal(form, "allianceColour"));
   elements.push(getRadioVal(form, "autoline"));
-  //new QRCode(document.getElementById("qrcode"), "http://jindo.dev.naver.com/collie");
+  elements.push(document.getElementById("ownershipScaleId").value)
+  elements.push(document.getElementById("ownershipSwitchId").value)
+  elements.push(document.getElementById("teleScaleId").value)
+  elements.push(document.getElementById("teleSwitchId").value)
+  elements.push(getRadioVal(form, "levitate"));
+  elements.push(getRadioVal(form, "force"));
+  elements.push(getRadioVal(form, "boost"));
+  elements.push(getRadioVal(form, "levitateActivated"));
+  elements.push(getRadioVal(form, "forceActivated"));
+  elements.push(getRadioVal(form, "boostActivated"));
+  elements.push(getRadioVal(form, "parkAchieved"));
+  elements.push(getRadioVal(form, "climbAchieved"));
+  elements.push(document.getElementById("foulId").value)
+  elements.push(document.getElementById("techFoulId").value)
+
+  elements.push(getRadioVal(form, "yellowCardReceived"));
+  elements.push(getRadioVal(form, "redCardReceived"));
+  elements.push(getRadioVal(form, "disabled"));
+  elements.push(getRadioVal(form, "disqualified"));
+  elements.push(getRadioVal(form, "winTieLoss"));
+  elements.push(document.getElementById("finalScoreId").value)
+  elements.push(document.getElementById("commentId").value)
+
   document.getElementById("qrcode").innerHTML = "";
 
   var qrcode = new QRCode(document.getElementById("qrcode"), {
@@ -34,6 +70,6 @@ function myFunction() {
   	colorLight : "#ffffff",
   	correctLevel : QRCode.CorrectLevel.H
   });
-
+  window.scrollTo(0, 0);
   alert(elements);
 }
